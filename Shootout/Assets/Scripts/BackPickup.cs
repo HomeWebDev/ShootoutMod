9d5b689feb7040218bd96e8d14d2bdad95167525
Klas Johnsson <klas.johnsson@alumni.chalmers.se>
2017-11-26 20:28:25 +0100

    Added head and back pickups

---
+++ Shootout/Assets/Scripts/BackPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackPickup : MonoBehaviour {
+
+    private Transform backTransform;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player1")
+        {
+            GameObject player1 = other.gameObject;
+
+            //Find back by reference from player object. Using "Find" by name seems sometimes buggy
+            backTransform = player1.transform.GetChild(7).GetChild(2).GetChild(0).GetChild(0).GetChild(0).transform;
+
+            transform.position = new Vector3(backTransform.position.x, backTransform.position.y, backTransform.position.z);
+            transform.localEulerAngles = new Vector3(0, 0, 0);
+            transform.parent = backTransform;
+        }
+    }
+}
---
+++ Shootout/Assets/Scripts/HeadPickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadPickup : MonoBehaviour
+{
+
+    private Transform headTransform;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player1")
+        {
+            GameObject player1 = other.gameObject;
+
+            //Find head by reference from player object. Using "Find" by name seems sometimes buggy
+            headTransform = player1.transform.GetChild(7).GetChild(2).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetChild(0).transform;
+
+            transform.position = new Vector3(headTransform.position.x, headTransform.position.y, headTransform.position.z);
+            transform.localEulerAngles = new Vector3(-90, 0, 0);
+            transform.parent = headTransform;
+        }
+    }
+}
