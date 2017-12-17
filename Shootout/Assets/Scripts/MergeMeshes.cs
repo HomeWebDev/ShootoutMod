d54945be21d8002c79a2f12289c6884af1772cf4
Klas Johnsson <klas.johnsson@alumni.chalmers.se>
2017-12-17 02:34:44 +0100

    Changed back from terrain to textured ground to improve FPS

---
+++ Shootout/Assets/Scripts/MergeMeshes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class MergeMeshes : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 position = transform.position;
+        transform.position = Vector3.zero;
+
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+            i++;
+        }
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        transform.gameObject.SetActive(true);
+
+        transform.position = position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
