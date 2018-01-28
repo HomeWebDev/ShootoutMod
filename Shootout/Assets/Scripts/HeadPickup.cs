42299250109d4acdccb682c68ec27aabaede4fd1
Klas Johnsson <klas.johnsson@alumni.chalmers.se>
2018-01-28 20:13:45 +0100

    Improved back and head pickup handling

--- Shootout/Assets/Scripts/HeadPickup.cs
+++ Shootout/Assets/Scripts/HeadPickup.cs
@@ -8,47 +8,47 @@ public class HeadPickup : MonoBehaviour
     private Transform headTransform;
     private GameObject player1;
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player1")
-        {
-            Debug.Log("pickup");
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Player1" && false)
+    //    {
+    //        Debug.Log("pickup");
 
-            //GameObject player1 = other.gameObject;
+    //        //GameObject player1 = other.gameObject;
 
-            //Find head by reference from player object. Using "Find" by name seems sometimes buggy
-            headTransform = player1.transform.GetChild(7).GetChild(2).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetChild(0).transform;
+    //        //Find head by reference from player object. Using "Find" by name seems sometimes buggy
+    //        headTransform = player1.transform.GetChild(7).GetChild(2).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetChild(0).transform;
 
-            //Remove all old back items if any
-            foreach (Transform child in headTransform)
-            {
-                if (child.tag == "HeadGear" && child != transform && child.GetComponent<HeadStats>().Group == GetComponent<HeadStats>().Group)
-                {
-                    //Debug.Log("delete");
-                    Destroy(child.gameObject);
-                }
-            }
+    //        //Remove all old head items if any
+    //        foreach (Transform child in headTransform)
+    //        {
+    //            if (child.tag == "HeadGear" && child != transform && child.GetComponent<HeadStats>().Group == GetComponent<HeadStats>().Group)
+    //            {
+    //                //Debug.Log("delete");
+    //                Destroy(child.gameObject);
+    //            }
+    //        }
 
-            transform.position = new Vector3(headTransform.position.x, headTransform.position.y, headTransform.position.z);
-            transform.localEulerAngles = new Vector3(-90, 0, 0);
-            transform.parent = headTransform;
+    //        transform.position = new Vector3(headTransform.position.x, headTransform.position.y, headTransform.position.z);
+    //        transform.localEulerAngles = new Vector3(-90, 0, 0);
+    //        transform.parent = headTransform;
 
-            UpdateHeadStats();
+    //        UpdateHeadStats();
 
-            //int group = GetComponent<HeadStats>().Group;
+    //        //int group = GetComponent<HeadStats>().Group;
 
-            ////Copy values from head stats to players headstats
-            //foreach (FieldInfo f in GetComponent<HeadStats>().GetType().GetFields())
-            //{
-            //    f.SetValue(player1.GetComponent<PlayerHeadStats>().HeadStats[group], f.GetValue(GetComponent<HeadStats>()));
-            //}
+    //        ////Copy values from head stats to players headstats
+    //        //foreach (FieldInfo f in GetComponent<HeadStats>().GetType().GetFields())
+    //        //{
+    //        //    f.SetValue(player1.GetComponent<PlayerHeadStats>().HeadStats[group], f.GetValue(GetComponent<HeadStats>()));
+    //        //}
 
-            //foreach (FieldInfo f in GetComponent<HeadStats>().GetType().GetFields())
-            //{
-            //    f.SetValue(player1.GetComponent<HeadStats>(), f.GetValue(GetComponent<HeadStats>()));
-            //}
-        }
-    }
+    //        //foreach (FieldInfo f in GetComponent<HeadStats>().GetType().GetFields())
+    //        //{
+    //        //    f.SetValue(player1.GetComponent<HeadStats>(), f.GetValue(GetComponent<HeadStats>()));
+    //        //}
+    //    }
+    //}
 
     void Start()
     {
